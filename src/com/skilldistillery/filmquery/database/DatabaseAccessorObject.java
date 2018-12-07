efe8d156de71426b85019dc9500c86777bfa120e
Jimmy Fruchterman <jimmyfruchterman@SDMacBookPro3-2.local>
2018-12-07 16:17:58 -0700

    initial commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,114 @@
+package com.skilldistillery.filmquery.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
+
+	@Override
+	public Film getFilmById(int filmId) {
+		Film film = null;
+		String user = "student";
+		String pass = "student";
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection(URL, user, pass);
+			// ...
+			String sql = "SELECT id, title, description, release_year, language_id, rental_duration, ";
+			sql += " rental_rate, length, replacement_cost, rating, special_features " + " FROM film "
+					+ " WHERE id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, filmId);
+			ResultSet filmResult = stmt.executeQuery();
+			if (filmResult.next()) {
+				film = new Film(); // Create the object
+				// Here is our mapping of query columns to our object fields:
+				film.setiD(filmResult.getInt(1));
+				film.setTitle(filmResult.getString(2));
+				film.setDescription(filmResult.getString(3));
+				film.setReleaseYear(filmResult.getInt(4));
+				film.setLanguageId(filmResult.getInt(5));
+				film.setRentalDuration(filmResult.getInt(6));
+				film.setRentalRate(filmResult.getDouble(7));
+				film.setLength(filmResult.getInt(8));
+				film.setReplacementCost(filmResult.getDouble(9));
+				film.setRating(filmResult.getString(10));
+				film.setSpecialFeatures(filmResult.getString(11));
+				film.setActors(getActorsByFilmId(filmId));
+			}
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// ...
+		return film;
+	}
+
+	@Override
+	public Actor getActorById(int actorId) {
+		Actor actor = null;
+		String user = "student";
+		String pass = "student";
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection(URL, user, pass);
+		// ...
+		String sql = "SELECT id, first_name, last_name FROM actor WHERE id = ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, actorId);
+		ResultSet actorResult = stmt.executeQuery();
+		if (actorResult.next()) {
+			actor = new Actor(); // Create the object
+			// Here is our mapping of query columns to our object fields:
+			actor.setiD(actorResult.getInt(1));
+			actor.setFirstName(actorResult.getString(2));
+			actor.setLastName(actorResult.getString(3));
+		}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// ...
+		return actor;
+	}
+
+	@Override
+	public List<Actor> getActorsByFilmId(int filmId) {
+		List<Actor> actors = new ArrayList<>();
+		String user = "student";
+		String pass = "student";
+		try {
+			Connection conn = DriverManager.getConnection(URL, user, pass);
+			String sql = "SELECT actor.id, first_name, last_name "
+					+ " FROM film JOIN film_actor ON film.id = film_actor.film_id "
+					+ " JOIN actor ON actor.id = film_actor.actor_id"
+					+ " WHERE film_id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, filmId);
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				int iD = rs.getInt(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				Actor actor = new Actor(iD, firstName, lastName);
+				actors.add(actor);
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return actors;
+	}
+
+}
